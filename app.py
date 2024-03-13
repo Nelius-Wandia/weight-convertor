ff410d55b117897872ca142fe1911bc7d664928c
Nelius-Wandia <151659348+Nelius-Wandia@users.noreply.github.com>
2024-03-13 16:55:59 +0300

    coded the users inputs and the conversion of kg to grams

---
+++ app.py
@@ -0,0 +1,6 @@
+weight = int(input('What is your weight? '))
+unit = input('(K)kgs or (G)grams ')
+
+if unit.upper() == 'K':
+    final_weight = weight * 1000
+    print(f"You are {final_weight} grams")
